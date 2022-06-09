b5f9e62b39a304b60af3c7d92f53ac85d0d266fc
theJoseNeto <joseneto180201@gmail.com>
2022-06-09 18:07:06 -0300

    bugs no recebimento das mensagens do chat com o bot corrigido

--- bot-telegram/index.js
+++ bot-telegram/index.js
@@ -1,63 +1,89 @@
 const axios = require("axios").default;
 
-class TelegramBot { 
-    
-    constructor(token, chatID){ 
-        this.token = token; 
+class TelegramBot {
+
+    constructor(token, chatID) {
+        this.token = token;
         this.chatID = chatID;
         this.apiConnectionUrl = `https://api.telegram.org/bot${token}`;
-        this.answeredMessages = []; 
+        this.answeredMessages = [];
     }
 
-    getMessageData = async() => {
 
-        const message = await axios.get(`${this.apiConnectionUrl}/getUpdates`);
-        const messageData = {};
-        
-        const isAMessage = "text" === message['result'][-1]["message"];
-        
-        if (message.length > 0 && isAMessage){
-            messageData["data"] = message
-            messageData["update_id"] = message["result"][-1]["update_id"]
-            messageData["message_id"] = message["result"][-1]["message"]["message_id"]
-            messageData["message_from"] = message["result"][-1]["message"]["from"]["first_name"]
-            messageData["message_text"]  = message["result"][-1]["message"]["text"]
+    messageWatcher = () => {
 
-            return messageData
+        const message_data = this.getMessageData()
+
+        if (message_data == {}) return;
+
+        const message = message_data["message_text"];
+
+
+        const update_id = message_data["update_id"];
+        const all_right = !this.answeredMessages[0] === update_id;
+
+        if (all_right) {
+            const response = "hello world";
+            this.sendMessage(response);
+            this.answered_messages[0] = update_id;
         }
-    
-    } 
+    }
+
+    getMessageData = async () => {
+
+        return await axios.get(`${this.apiConnectionUrl}/getUpdates`).then( message => {
+
+            const messageData = {};
+
+            // const isAMessage = "text" === message['result'][-1]["message"];
+            const lastIndex = message.data.result.length - 1;
+            
+            // console.log(message.data.result[lastIndex]["update_id"])
+            if (message.data.result.length > 0) {
+                messageData["data"] = message;
+                messageData["update_id"] = message.data["result"][lastIndex]["update_id"];
+                messageData["message_id"] = message.data["result"][lastIndex]["message"]["message_id"];
+                messageData["message_from"] = message.data["result"][lastIndex]["message"]["from"]["first_name"];
+                messageData["message_text"] = message.data["result"][lastIndex]["message"]["text"];
+
+            }
+
+            return messageData;
+
+        });
+
+    }
 
     chatCheck = async chatId => {
         let isCorrectChat = false
         if (!this.chatID == chatId) isCorrectChat = true;
-        return isCorrectChat
+        return isCorrectChat;
     }
 
 
     getChatInfos = messageData => {
-        if( messageData == {}) return;
+        if (messageData == {}) return;
 
-        let chatInfos = {}        
-        let chatType = messageData["data"]["result"][-1]["message"]["chat"]["type"]
-        let chatTitle = messageData["data"]["result"][-1]["message"]["chat"]["title"]
-        let chatId = messageData["data"]["result"][-1]["message"]["chat"]["id"]
+        let chatInfos = {};
+        let chatType = messageData["data"]["result"][-1]["message"]["chat"]["type"];
+        let chatTitle = messageData["data"]["result"][-1]["message"]["chat"]["title"];
+        let chatId = messageData["data"]["result"][-1]["message"]["chat"]["id"];
 
         const is_correct_chat = this.chat_check(chatId);
 
-        if (is_correct_chat){
+        if (is_correct_chat) {
             chatInfos["chat_id"] = chatId;
             chatInfos["chat_type"] = chatType;
             chatInfos["chat_title"] = chatTitle;
         }
 
-        return chatInfos
+        return chatInfos;
     }
 
     sendMessage = async text => {
         let chatID = this.chat_id;
-        const url = `{self.api_connection_url}/sendMessage?chat_id=${chatID}&text=${text}`;
-        await axios.get(url)
+        const url = `${this.apiConnectionUrl}/sendMessage?chat_id=${chatID}&text=${text}`;
+        await axios.get(url);
     }
 
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const fs = require("fs").promises;
 const {resolve} = require('path');
 const TelegramBot = require("./bot-telegram");
@@ -5,14 +6,15 @@ const TelegramBot = require("./bot-telegram");
 const data = "console.log('hello world!')"; // Message from telegram;
 
 const token = process.env.TELEGRAM_URL_CONNECTION;
-const bot = new TelegramBot(token, "chat_id");
+const bot = new TelegramBot(token, '-1001387626450');
 
+const teste = () => bot.getMessageData().then( r => console.log(r));
+teste();
 
-const createFile = async (fileName, typeFile, content) => {
-    const outputPath = resolve(__dirname, "output");
-    await fs.writeFile(resolve(outputPath,`${fileName}.${typeFile}`), content); 
+// const createFile = async (fileName, typeFile, content) => {
+//     const outputPath = resolve(__dirname, "output");
+//     await fs.writeFile(resolve(outputPath,`${fileName}.${typeFile}`), content); 
+// }
 
-}
-
-createFile('test', "js", data); 
+// createFile('test', "js", data); 
 
